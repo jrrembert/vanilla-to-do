f85b3c9ac38ab64f31450d90da40d6c3499fff25
J. Ryan Rembert <j.ryan.rembert@gmail.com>
2015-06-02 13:32:13 -0700

    More scaffolding

---
+++ js/app.js
@@ -0,0 +1,67 @@
+//Problem: User interaction doesn't provide desired results.
+//Solution: Add interactivity so the user can manage daily tasks.
+
+
+//Add a new task
+
+var createTask = function() {
+
+
+    //When button is pressed, create task
+    //Create new list item with text from #new-task
+        //input (checkbox)
+        //label
+        //input (text)
+        //button.edit
+        //button.delete
+        //EAch elements, needs modifed and appended
+
+
+}
+
+
+
+//Edit an existing task
+
+
+var editTask = function () {
+
+    //When edit button is pressed
+        //if class of parent is .editMode
+            //Switch from .editMode
+            //label text become the input's value
+        //else
+            //Switch to .editMode
+            //input value becomes the label's text
+
+    //Toggle .editMode
+
+}
+
+//Delete a task
+
+
+var deleteTask = function () {
+    //When delete button is pressed
+        //Remove the parent list item from the ul
+
+}
+
+
+
+//Mark a task as complete
+
+
+var completeTask = function () {
+    //When checkbox is checked
+        //Append list item to #completed-tasks
+}
+
+
+//Mark a task as incomplete
+
+var incompleteTask = function () {
+    //When checkbox is unchecked
+        //
+
+}
